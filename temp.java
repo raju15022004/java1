2d1138531f598cfc8958191313456a68181f125f
raju15022004 <raju032004@gmail.com>
2026-02-11 00:40:33 +0530

    temp.java

---
+++ temp.java
@@ -0,0 +1,18 @@
+public class temp{
+
+  // public static void main(String[] args) {
+      int x=10;
+      void show(int x){
+        System.out.print(x);
+        System.out.print(x);
+      }
+
+      public static void main(String[] args) {
+          temp t1=new temp();
+          t1.show(20);
+
+
+      }
+
+  // }
+}
