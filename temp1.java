015a35cb5a1aa50ca6d8c28799e4410e314f943f
raju15022004 <raju032004@gmail.com>
2026-02-13 00:47:26 +0530

    temp1.java

---
+++ temp1.java
@@ -0,0 +1,27 @@
+public class temp1{
+
+  // int x=10;
+  // void show(int y,temp z){
+  //   System.out.println(z);
+  //   System.out.println(x);
+  // }
+
+  // public static void main(String[] args) {
+  //     temp t1=new temp();
+  //     t1.show(20);
+  //     System.out.println(t1);
+  // }
+
+
+   int x=10;
+   void show(int y){
+    System.out.println(x);
+    System.out.println(y);
+   }
+
+   public static void main(String[] args) {
+       temp t1=new temp();
+       t1.show(20);
+   }
+
+}
