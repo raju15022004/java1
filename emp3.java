3ac6c55ad6c846bbcfc829740bae038645edddf5
raju15022004 <raju032004@gmail.com>
2026-02-14 01:22:28 +0530

    emp3.java

---
+++ emp3.java
@@ -0,0 +1,20 @@
+public class emp3{
+
+  int salary;
+  emp3(){
+  salary=15000;
+  }
+
+  void show(){
+    System.out.println(salary);
+  }
+
+  public static void main(String[] args) {
+      // System.out.println();
+      emp e1 = new emp();
+        e1.show();
+
+  }
+
+
+}
