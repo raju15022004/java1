34694412e628007916a63bfc9d625c13e557d4e2
raju15022004 <raju032004@gmail.com>
2026-02-14 01:24:29 +0530

    temp2.java

---
+++ temp2.java
@@ -0,0 +1,14 @@
+public class temp2{
+
+  temp2(){
+    System.out.println("default");
+  }
+
+  void temp2(){
+    System.out.println("hello");
+  }
+  public static void main(String[] args) {
+      new temp2();
+  }
+
+}
