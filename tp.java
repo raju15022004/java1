0597c0053e428a7e8f210722d1078f8bb1541cef
raju15022004 <raju032004@gmail.com>
2026-02-14 00:27:01 +0530

    tp.java

--- tp.java
+++ tp.java
@@ -1,23 +1,50 @@
+// public class tp{
+
+//   int x;
+//   int y;
+
+//   // temp(){
+//   //  x=20;
+//   //  y=30;
+//   // }
+
+//   void show(){
+//     System.out.println(x);
+//     System.out.println(y);
+//   }
+
+//   public static void main(String[] args) {
+//       temp t1=new temp();
+//       t1.show(10);
+
+//       temp t2=new temp();
+//       t2.show(25);
+//   }
+// }
+
+
+//////////////////////////////////////
+
 public class tp{
 
-  int x;
-  int y;
+  int x=getx();
+  int getx()
+{
+  System.out.println(x);
+  return 20;
 
-  //  Temp(){
-  //  x=20;
-  //  y=30;
-  // }
-
-  void show(){
-    System.out.println(x);
-    System.out.println(y);
-  }
-
-  public static void main(String[] args) {
-      temp t1=new temp();
-      t1.show(10);
-
-      temp t2=new temp();
-      t2.show(25);
-  }
 }
+void temp()
+{
+  x=20;
+  x=getx();
+  System.out.println(x);
+}
+
+public static void main(String[] args){
+  temp t1=new temp();
+  t1.show(20);
+}
+}
+
+
