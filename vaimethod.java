b748346408183d23e1ae5cde3127c52e8cf6a5d6
raju15022004 <raju032004@gmail.com>
2026-02-22 18:48:43 +0530

    vaimethod.java

--- vaimethod.java
+++ vaimethod.java
@@ -1,15 +1,14 @@
 class vaimethod{
-  int x=this.get();
+  int x=get();
   int get(){
     System.out.println(x);
     return 20;
   }
-  vaimathod(){
-
-    System.out.println(x){
+  vaimethod(){
+    System.out.println(x);
     }
     public static void main(String[] args){
       new vaimethod();
     }
   }
-}
+
