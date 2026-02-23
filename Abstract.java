6a099a538d8092f8f2c6357fae1bf6e6415e5476
raju15022004 <raju032004@gmail.com>
2026-02-23 21:55:11 +0530

    Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,27 @@
+ class Base{
+
+  int x;
+  int y;
+  void show(){
+
+    System.out.println(x);
+    System.out.println(y);
+  }
+}
+
+class child extends Base{
+  void get(int x,int y){
+    this.x=x;
+    this.y=y;
+  }
+}
+
+public class Abstract{
+
+  public static void main(String[] args){
+
+    child c1=new child();
+    c1.get(10, 20);
+    c1.show();
+  }
+}
