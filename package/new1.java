49178e1cf77e62fde9ad25c9d6b60d6e648d6f4b
raju15022004 <raju032004@gmail.com>
2026-05-22 11:45:44 +0530

    new1.java

---
+++ package/new1.java
@@ -0,0 +1,16 @@
+// package package;
+
+public class new1 {
+  public static void main(String... s) {
+      try {
+        int x=10/s.length;
+        System.out.println(x);
+        int z[]=new int [s.length];
+
+      }
+      catch(Exception e){
+        System.out.println(e);
+      }
+  }
+
+}
