5932cccb2c5bbb46cf24dc52ca0a95d72ce1369d
raju15022004 <raju032004@gmail.com>
2026-05-25 12:15:01 +0530

    new1.java

--- package/new1.java
+++ package/new1.java
@@ -3,7 +3,7 @@
 public class new1 {
   public static void main(String... s) {
       try {
-        int x=10/s.length;
+        int x=20/s.length;
         System.out.println(x);
         int z[]=new int [s.length];
 
