ac87a7e8cdeb892196d2b8ff17876b9223df140a
raju15022004 <raju032004@gmail.com>
2026-05-28 00:56:31 +0530

    sortorder.java

---
+++ Assigment/singleoccurence.java
@@ -0,0 +1,26 @@
+public class singleoccurence {
+
+  static String singleoccurence(String s){
+    if(s == null || s.isEmpty()){
+      return "";
+
+    }
+
+    String result="";
+    result=result+s.charAt(0);
+
+    for(int i=1;i<s.length();i++){
+      if(s.charAt(i) !=s.charAt(i-1)){
+        result=result+s.charAt(i);
+      }
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+      String s="rraajjuu";
+      System.out.println(singleoccurence(s));
+  }
+
+}
---
+++ Assigment/sortorder.java
@@ -0,0 +1,28 @@
+public class sortorder {
+
+  static void sortorder(String s){
+    if(s == null || s.isEmpty()){
+      return;
+    }
+
+    char[] arr = s.toCharArray();
+
+    //bubble sort
+    for(int i=0;i<arr.length;i++){
+      for(int j=0;j<arr.length-1;j++){
+        if(arr[j]>arr[j+1]){
+          char temp = arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+        }
+      }
+    }
+    System.out.println(new String(arr));
+  }
+
+  public static void main(String[] args) {
+      String s="raju";
+      sortorder(s);
+  }
+
+}
