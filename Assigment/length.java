6338bd0ae1327a88429df3952f9ced4f6329b6ed
raju15022004 <raju032004@gmail.com>
2026-05-28 00:23:14 +0530

    sequenceCount.java

---
+++ Assigment/length.java
@@ -0,0 +1,21 @@
+public class length {
+
+  static int length(String s){
+    if(s == null){
+      return 0;
+    }
+
+    int count=0;
+
+    for(char ch : s.toCharArray()){
+      count++;
+    }
+
+    return count;
+  }
+
+  public static void main(String[] args) {
+      String s=" raju";
+      System.out.println(length(s));
+  }
+}
---
+++ Assigment/sequenceCount.java
@@ -0,0 +1,31 @@
+public class sequenceCount {
+
+  static void sequenceCount(String s){
+    if(s == null || s.isEmpty()){
+      return;
+    }
+
+    int[] count = new int [256];
+
+    //count store karo
+    for(int i=0;i<s.length();i++){
+      count[s.charAt(i)]++;
+    }
+
+    //print unique characters + count
+    for(int i=0;i<s.length();i++){
+      char ch=s.charAt(i);
+
+      if(count[ch] !=0){
+        System.out.println(ch + " = " + count[ch]);
+        count[ch]=0;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+      String s="raju";
+      System.out.println(s);
+  }
+
+}
