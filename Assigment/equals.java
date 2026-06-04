2fadad34d9689682ad2f043ad7b86cba57ecfb48
Raju kumar sahani <157303151+raju15022004@users.noreply.github.com>
2026-06-04 18:53:37 +0530

    Change string s1 from 'raju' to 'raj'

--- Assigment/equals.java
+++ Assigment/equals.java
@@ -20,7 +20,7 @@ public class equals {
   }
 
   public static void main(String[] args) {
-      String s1="raju";
+      String s1="raj";
       String s2="raju";
       System.out.println(equals(s1, s2));
   }
