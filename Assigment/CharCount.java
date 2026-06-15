7ea862e3616ed2c1d7c23b8191161ddddf3ad640
Raju kumar sahani <157303151+raju15022004@users.noreply.github.com>
2026-06-15 20:08:26 +0530

    Update string in main method to include 'world'

--- Assigment/CharCount.java
+++ Assigment/CharCount.java
@@ -17,7 +17,7 @@ public class CharCount {
     return count;
   }
   public static void main(String[] args) {
-      String s="Hello  ";
+      String s="Hello world ";
       System.out.println(CharCount(s));
   }
 
