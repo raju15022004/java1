cbc6ea974e7758da2786128f59dbb6f8136942bf
Raju kumar sahani <157303151+raju15022004@users.noreply.github.com>
2026-07-04 14:04:28 +0530

    Add '8' to the input string for reversal

--- Assigment/reverse.java
+++ Assigment/reverse.java
@@ -17,7 +17,7 @@ public class reverse {
 
   }
   public static void main(String[] args) {
-      String s="1,2,3,4,4,5,6,7";
+      String s="1,2,3,4,4,5,6,7,8";
       System.out.println(reverse(s));
   }
 
