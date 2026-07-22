3aea4c76674118ffb0ea55047c923425da8c434f
raju15022004 <raju032004@gmail.com>
2026-07-22 14:18:14 +0530

    Hash.java

--- Linkedlist/Hash.java
+++ Linkedlist/Hash.java
@@ -7,7 +7,7 @@ class Hash{
       System.out.println(al.size());
       al.add("a");
       al.add("b");
-      al.add("c");
+      al.add("d");
       System.out.println(al.add("c"));
       System.out.println(al.size());
       System.out.println(al);
