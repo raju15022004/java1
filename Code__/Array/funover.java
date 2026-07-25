7f1a5845774fabdb5f673e3db1afde3b836fe5e7
raju15022004 <raju032004@gmail.com>
2026-07-26 01:12:18 +0530

    fun.java

---
+++ Code__/Array/funover.java
@@ -0,0 +1,12 @@
+public class funover {
+  public static void main(String[] args) {
+
+    int x=sum(2,3);
+    System.out.println(x);
+  }
+
+  static int sum(int a, int b) {
+      return (a+b);
+  }
+
+}
