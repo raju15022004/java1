dc2abaf9feb438ad5afcf762121a66d38420dfc8
raju15022004 <raju032004@gmail.com>
2026-08-03 13:00:39 +0530

    autoboxing .java

---
+++ Code__/Variable/autoboxing.java
@@ -0,0 +1,14 @@
+// package Variable;
+
+public class autoboxing {
+
+  public static void main(String[] args){
+
+    int x=10;
+    Integer y=Integer.valueOf(x);
+
+    System.out.println(x);
+    System.out.println(y);
+  }
+
+}
