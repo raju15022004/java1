98b8a68b44239c2c60e9d1c42c3c8ab01b6247b6
raju15022004 <raju032004@gmail.com>
2026-08-08 15:55:34 +0530

    demo.java

---
+++ Code__/String/demo.java
@@ -0,0 +1,18 @@
+// package String;
+
+public class demo {
+
+  public static void main(String[] args){
+
+    String s1="Hello";
+    String s2="Hello";
+
+    // System.out.println(s1 == s2);
+
+    String s3=new String("Raju");
+    String s4=new String("Raju");
+
+    System.out.println(s3 == s4);
+  }
+
+}
---
+++ Code__/String/equals.java
@@ -0,0 +1,19 @@
+// package String;
+
+public class equals {
+
+  public static void main(String[] args) {
+      String s1="ja"+"va";
+      String s2="java";
+
+      // System.out.println(s1 == s2);
+
+      String s3="hello";
+
+      String s4=s3 +"world";
+      String s5="hello world";
+
+      System.out.println(s4 == s5);
+  }
+
+}
