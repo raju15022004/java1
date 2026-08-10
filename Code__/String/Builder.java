91d81928140be6f0aac41ed8c82457a625135836
raju15022004 <raju032004@gmail.com>
2026-08-10 14:36:58 +0530

    Builder.java

---
+++ Code__/String/Builder.java
@@ -0,0 +1,28 @@
+public class Builder {
+
+  public static void main(String[] args) {
+      StringBuilder sb=new StringBuilder();
+
+      sb.append("Raju");
+      // sb.append(" kumar");
+
+      // System.out.println(sb);
+
+      // Insert
+
+      // sb.insert(3, 'a');
+      // System.out.println(sb);
+
+      // delect
+      // sb.delete(0, 2);
+      // System.out.println(sb);
+
+      // repalce
+
+      // sb.replace(1, 2, "b");
+      // System.out.println(sb);
+
+      System.out.println(sb.length());
+  }
+
+}
--- Code__/String/methods1.java
+++ Code__/String/methods1.java
@@ -2,21 +2,61 @@ public class methods1 {
 
   public static void main(String[] args) {
       String s1=new String("Raju");
-      String s2=new String("RAJU");
+      String s2=new String("Raju");
 
       // Length
-      System.out.println(s1.length());
-      System.out.println(s1.isEmpty());
-      System.out.println(s1.isBlank());
+      // System.out.println(s1.length());
+      // System.out.println(s1.isEmpty());
+      // System.out.println(s1.isBlank());
 
       // character access
-
-      System.out.println(s1.charAt(2));
-      char[] arr=s1.toCharArray();
+      // System.out.println(s1.charAt(2));
+      // char[] arr=s1.toCharArray();
 
       // Comparison
-      System.out.println(s1.equals(s2));
-      System.out.println(s1.equalsIgnoreCase(s2));
+      // System.out.println(s1.equals(s2));
+      // System.out.println(s1.equalsIgnoreCase(s2));
+
+      // Lexicographical comparison --> Dictionary
+      // System.out.println(s1.compareTo(s2));
+
+      // Searching
+      // System.out.println(s1.contains("ju"));
+      // System.out.println(s1.indexOf("ju"));
+      // System.out.println(s1.lastIndexOf("ju"));
+      // System.out.println(s1.startsWith("Ra"));
+
+      // Extractioon / transformation
+      // System.out.println(s1.substring(1));
+      // System.out.println(s1.toUpperCase());
+      // System.out.println(s1.trim());
+
+      // System.out.println(s1.replace('a', 'j'));
+
+      // String s3="Raju,sonam";
+      // String[] arr=s3.split(",");
+
+      // for(String s: arr){
+      //   System.out.println(s);
+
+      // }
+
+      // System.out.println(String.join("-", "a","b","c"));
+
+      // Conversion
+      // String s4=new String(String.valueOf(4));
+
+      // byte[] arr=s1.getBytes();
+      // for(byte i : arr){
+      //   System.out.print(i + " ,");
+      // }
+
+      // Advance --> intern(), format()
+
+      String s5=new String("hello");
+      String s6=s5.intern();
+
+      System.out.println(s5 == s6);
   }
 
 }
