67bc3e6fddede06584863e27ce19286b916503d8
raju15022004 <raju032004@gmail.com>
2026-08-10 15:49:14 +0530

    Generics.java

--- Code__/String/Generics.java
+++ Code__/String/Generics.java
@@ -2,10 +2,40 @@ public class Generics {
 
   public static void main(String[] args) {
 
-    String s="Hello";
-    Object obj=s;
+    // String s="Hello";
+    // Object obj=s;
+    // System.out.println(obj);
 
-    System.out.println(obj);
+    Box b1=new Box(10);
+    Box b2=new Box("Hello");
+    Box b3=new Box(true);
+
+    //Downcasting
+    Integer x=(Integer) b1.getValue();
+    String s=(String) b2.getValue();
+    Boolean b=(Boolean) b3.getValue();
+
+    System.out.println(x + 5);
+    System.out.println(s + 5);
+    System.out.println(b);
+
+
   }
 
+}
+
+class Box{
+  private Object value;
+
+  Box(Object value){
+    this.value=value;
+  }
+
+  public Object getValue(){
+    return this.value;
+  }
+
+  public void setValue(Object value){
+    this.value=value;
+  }
 }
