a90ecab940a3dc5819d4e17e98ecdd58d868d3ae
raju15022004 <raju032004@gmail.com>
2026-08-11 22:38:15 +0530

    builder1.java

--- Code__/String/Generics1.java
+++ Code__/String/Generics1.java
@@ -1,28 +1,69 @@
 
 
-public class Generics1 {
+// public class Generics1 {
 
-  public static void main(String[] args) {
+//   public static void main(String[] args) {
 
-    Box<Integer> b1=new Box<>(10);
+//     Box<Integer> b1=new Box<>(10);
 
-System.out.println(b1.getValue() +5);
+// System.out.println(b1.getValue() +5);
+//   }
+
+// }
+
+// class Box <T>{
+//   private T value;
+
+//   Box(T value){
+//     this.value=value;
+//   }
+
+//   public T getValue(){
+//     return this.value;
+//   }
+
+//   public void setValue(T value){
+//     this.value=value;
+//   }
+// }
+
+public class Generics1{
+
+  static <T extends Animal> void test(T animal){
+  animal.eat();
+}
+
+
+   public static void main(String[] args) {
+
+
+
+    Animal a=new Animal();
+    Dog d=new Dog();
+
+    test(a);
+    test(d);
+
+
+  }
+}
+
+/**
+ * InnerGenerics1
+ */
+
+
+ class Animal {
+
+  void eat(){
+    System.out.println("Animal is eating");
   }
 
 }
 
-class Box <T>{
-  private T value;
-
-  Box(T value){
-    this.value=value;
-  }
-
-  public T getValue(){
-    return this.value;
-  }
-
-  public void setValue(T value){
-    this.value=value;
+class Dog extends Animal{
+  void bark(){
+    System.out.println("Dog is ");
   }
 }
+
---
+++ Code__/String/builder1.java
@@ -0,0 +1,29 @@
+public class builder1 {
+
+  public static void main(String[] args) {
+
+
+// Delect
+    // StringBuilder sb=new StringBuilder("Raju Kumar");
+    // sb.delete(5, 10);
+    // System.out.println(sb);
+
+    // replace
+  // StringBuilder sb =new StringBuilder("Raju Kumar");
+  // sb.replace(5, 12, "Sahani");
+  // System.out.println(sb);
+
+  //reverse
+  // String str="Raju";
+  // StringBuilder sb=new StringBuilder(str);
+  // sb.reverse();
+  // System.out.println(sb);
+
+  
+
+
+
+
+  }
+
+}
