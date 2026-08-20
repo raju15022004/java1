c19dd4f8aa594040f03976e783d00e5c7aeb8da3
raju15022004 <raju032004@gmail.com>
2026-08-20 16:06:12 +0530

    compare1.java

---
+++ Code__/Queue/compare1.java
@@ -0,0 +1,33 @@
+import java.util.TreeSet;
+
+public class compare1 {
+  public static void main(String[] args) {
+
+    Student s1=new Student("raju", 60);
+    Student s2=new Student("Rohit", 60);
+
+    TreeSet<Student> set =new TreeSet<>();
+
+    set.add(s1);
+    set.add(s2);
+
+    System.out.println(set.size());
+  }
+
+}
+
+class Student implements Comparable<Student>{
+  String name;
+  int marks;
+
+  public Student(String name,int marks){
+    this.name=name;
+    this.marks=marks;
+  }
+
+  @Override
+  public  int compareTo(Student other){
+    return this.marks - other.marks;
+  }
+}
+
