2644999b0aef6fc717aff57f2ba4e29729aacac8
raju15022004 <raju032004@gmail.com>
2026-08-21 14:47:09 +0530

    function1.java

--- Code__/Queue/function1.java
+++ Code__/Queue/function1.java
@@ -1,4 +1,22 @@
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+
 public class function1 {
-  
+  public static void main(String[] args) {
+
+    Function<Integer, Integer> square=x -> x * x;
+
+    // System.out.println(square.apply(5));
+
+    Consumer<Integer> print = x -> System.out.println(x);
+    // print.accept(7);
+
+    Supplier<Double> randomValue=() -> Math.random();
+
+    System.out.println(randomValue.get());
+  }
 
 }
