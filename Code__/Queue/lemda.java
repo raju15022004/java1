9180b3c9b27d6a642e05e6e5d5aa6af4acd50370
raju15022004 <raju032004@gmail.com>
2026-08-21 11:34:57 +0530

    lemda.java

---
+++ Code__/Queue/lemda.java
@@ -0,0 +1,22 @@
+public class lemda {
+  public static void main(String[] args){
+    Calculator c=new Addition();
+    int sum=c.Calculator(5, 4);
+
+    System.out.println(sum);
+
+  }
+
+}
+
+@FunctionalInterface
+interface Calculator{
+  int Calculator(int a,int b);
+}
+
+class Addition implements Calculator{
+  @Override
+  public int Calculator(int a,int b){
+    return a+b;
+  }
+}
