cbf96c992eb789b67cc5db4dd9924b9482759de9
raju15022004 <raju032004@gmail.com>
2026-08-22 13:33:48 +0530

    stream1.java

---
+++ Code__/Queue/stream1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class stream1{
+   public static void main(String[] args) {
+
+    List<Integer> list=new ArrayList(List.of(5,12,7,14));
+
+    Stream<Integer> s=list.stream();
+    s=s.filter(x -> x >10);
+    s=s.map(x -> x *2);
+    s.forEach(System.out::println);
+
+  }
+}
