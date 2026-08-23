0edb31ea1eb3ba7cc66726bda68e3242e01687aa
raju15022004 <raju032004@gmail.com>
2026-08-23 20:27:15 +0530

    sream1.java

--- Code__/Queue/stream1.java
+++ Code__/Queue/stream1.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class stream1{
    public static void main(String[] args) {
 
-    List<Integer> list=new ArrayList(List.of(5,12,7,14));
+    List<Integer> list=new ArrayList(List.of(5,12,7,10));
 
     Stream<Integer> s=list.stream();
     s=s.filter(x -> x >10);
