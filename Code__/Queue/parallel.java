5817c0bdb313c93da3ae01a5ccdf9d9034f9dc05
raju15022004 <raju032004@gmail.com>
2026-08-25 13:19:25 +0530

    Add parallel example

---
+++ Code__/Queue/parallel.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class parallel {
+  public static void main(String[] args) {
+      List<Integer> list=new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,10,11));
+
+      list.stream()
+           .map(x -> x * 2)
+           .forEach(System.out::println);
+
+           list.parallelStream()
+                .map(x -> x * 2)
+                .forEach(System.out::println);
+
+  }
+
+
+}
