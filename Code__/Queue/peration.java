8dab015c312ab02965d1f7087b07d46a075702c6
raju15022004 <raju032004@gmail.com>
2026-08-25 10:46:11 +0530

    Add Optional example in Java

---
+++ Code__/Queue/peration.java
@@ -0,0 +1,16 @@
+
+import java.util.Optional;
+
+public class peration {
+
+  public static void main(String[] args) {
+    Optional<String> name=getName();
+
+    System.out.println("Unknown");
+  }
+
+  public static Optional<String> getName(){
+    return Optional.ofNullable("Raju");
+  }
+
+}
