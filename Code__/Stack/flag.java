5050942eacde876fee415f45c4955c518a462347
raju15022004 <raju032004@gmail.com>
2026-08-31 15:07:04 +0530

    flag.java

---
+++ Code__/Stack/flag.java
@@ -0,0 +1,44 @@
+public class flag{
+
+  public static void main(String[] args) {
+    Box box=new Box();
+    Thread t1=new Thread(()->{
+      for(int i=1;i<=20;i++){
+        try{
+          Thread.sleep(100);
+        }catch(Exception e){}
+        box.producer(i);
+      }
+    });
+    
+    Thread t2=new Thread(()->{
+      for(int i=1;i<=20;i++){
+        try{
+          Thread.sleep(100);
+        }catch(Exception e){}
+        box.producer(i);
+      }
+    });
+
+    t1.start();
+    t2.start();
+
+  }
+}
+
+class Box{
+  Integer item;
+  Boolean flag=false;
+
+  void producer(int value){
+    item=value;
+    flag=true;
+    System.out.println("Producer produces"+item);
+  }
+
+  void consumer(){
+    System.out.println("Consumer consumes"+item);
+    item=null;
+    flag=false;
+  }
+}
