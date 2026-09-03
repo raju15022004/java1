1920ad0b62198a4f1f02a471b8e1de2af589c4ba
raju15022004 <raju032004@gmail.com>
2026-09-03 14:44:07 +0530

    Executor.java

---
+++ Code__/Stack/Executor.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Executor{
+
+  public static void main(String[] args) {
+
+    ExecutorService executor=Executors.newFixedThreadPool(2);
+
+    for(int i=1; i<5; i++){
+
+      int taskId=i;
+
+      executor.execute(()->{
+        System.out.println("Task"+taskId+"is perfromed by"+Thread.currentThread().getName());
+
+      });
+    }
+    executor.shutdown();
+  }
+}
