3b315cd54073d163a377c49382dc80c3a22030b1
raju15022004 <raju032004@gmail.com>
2026-09-26 11:00:44 +0530

    String.java

--- Code__/Array/String1.java
+++ Code__/Array/String1.java
@@ -3,7 +3,7 @@
 public class String1 {
   public static void main(String[] args) {
       String firstName="Raju";
-      String lastName="kumar";
+      String lastName="Sahani";
 
       System.out.println(firstName +" " + lastName);
   }
